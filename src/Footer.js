7b204fc168248ec23314d33e467179c901efcf55
Kirandeep Kaur <me@kiran.cc>
2020-04-12 00:51:52 +0530

    footer changes

--- src/Footer.js
+++ src/Footer.js
@@ -2,9 +2,16 @@ import React from "react";
 
 const Footer = () => {
   return (
-  <div className="footer">
+    <div className="footer">
       <div className="advice">Stay Home Stay Safe </div>
-      <div className="created_by">  By: Kirandeep Kaur </div>
+      <div className="created_by">
+        {" "}
+        By: Kirandeep Kaur{" "}
+        <span>
+          {" "}
+          Join <a href="https://t.me/kiran433">telegram channel</a>
+        </span>{" "}
+      </div>
     </div>
   );
 };
