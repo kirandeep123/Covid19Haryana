7bb81b11288f19ea844b14055274e0326bf02545
Kirandeep Kaur <me@kiran.cc>
2020-04-11 20:19:28 +0530

    Updates

---
+++ precache-manifest.c24e563255d50c7abcdbf29541add26e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c804840362f7ac8e65269af81cb21b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f2ce330b58bfcc4bf65",
+    "url": "/static/css/main.6fc0059a.chunk.css"
+  },
+  {
+    "revision": "03cbf47e8cd337ebbeb1",
+    "url": "/static/js/3.164f6a79.chunk.js"
+  },
+  {
+    "revision": "5289d21bca6647f2d1c9",
+    "url": "/static/js/4.1655e962.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/4.1655e962.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eab814f044a3dfffb25",
+    "url": "/static/js/5.c293f99f.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/static/js/5.c293f99f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3ba2a11c8d1283a89ba",
+    "url": "/static/js/6.74f8ec0a.chunk.js"
+  },
+  {
+    "revision": "c2703eee005e82dd04f5f76ec1204181",
+    "url": "/static/js/6.74f8ec0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f2ce330b58bfcc4bf65",
+    "url": "/static/js/main.4f10b3d5.chunk.js"
+  },
+  {
+    "revision": "3c7fa8d07473062a79a9",
+    "url": "/static/js/runtime-main.6839bd1e.js"
+  },
+  {
+    "revision": "a77b6bab2a58c63a8451",
+    "url": "/static/js/xlsx.3690751f.chunk.js"
+  }
+]);
