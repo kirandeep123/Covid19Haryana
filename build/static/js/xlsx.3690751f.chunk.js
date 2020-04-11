997d91942341860449bbecc0c1bc1a3a6f0ca9f5
Kirandeep Kaur <me@kiran.cc>
2020-04-11 23:30:33 +0530

    build changes

---
+++ build/static/js/xlsx.3690751f.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcovidharyana=this.webpackJsonpcovidharyana||[]).push([[2],{262:function(n,a){},350:function(n,a){},351:function(n,a){}}]);
+//# sourceMappingURL=xlsx.3690751f.chunk.js.map
